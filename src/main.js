17b720bb02f4920eccb07c81eb2c80f3499637d4
kainenw <108428180+kainenw@users.noreply.github.com>
2025-08-25 20:30:06 -0500

    Initialize project skeleton

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { App } from './app/App.js';
+
+const root = document.getElementById('app');
+const app = new App(root);
+app.start();
---
+++ src/render/sceneManager.js
@@ -0,0 +1,9 @@
+export class SceneManager {
+  constructor(scene, renderer) {
+    this.scene = scene;
+    this.renderer = renderer;
+  }
+  update(dt) {
+    // TODO: update scene objects
+  }
+}
